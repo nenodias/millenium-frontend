a25c28c7054615cde9e643bf970cf556fb25d5f8
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-09-27 12:24:57 -0300

    Creating all the pages and forms

---
+++ src/routes/falha/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to /falha</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/montadora/form/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to /montadora/form</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
