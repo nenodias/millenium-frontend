d6d9a45b8609acaa353dd8c8e258e6df053b6b28
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-09-27 14:08:28 -0300

    Adding bulma and font-awesome 4.7.0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,10 @@
-<slot></slot>
+<script lang="ts">
+    import "bulma/css/bulma.min.css";
+    import "font-awesome/css/font-awesome.min.css";
+</script>
+
+<svelte:head>
+    <title>Millenium</title>
+</svelte:head>
+
+<slot />
