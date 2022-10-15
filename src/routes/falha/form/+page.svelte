c1334d780099b7facfb37a05e8a2c3fe436ed0ce
nenodias <horacio.dias92@gmail.com>
2022-10-14 22:16:03 -0300

    Removing reactive block, because everytime it was runned and model was static.

--- src/routes/falha/form/+page.svelte
+++ src/routes/falha/form/+page.svelte
@@ -12,23 +12,18 @@
         descricao: "",
     };
 
-    $: {
-        page.subscribe(async (data) => {
-            if (data.url.searchParams.has("id")) {
-                const text = data.url.searchParams.get("id");
-                if (text) {
-                    model.id = parseInt(text);
-                    let resp = await client.findById(
-                        model.id,
-                        token.getToken()
-                    );
-                    if (resp) {
-                        model = resp;
-                    }
+    page.subscribe(async (data) => {
+        if (data.url.searchParams.has("id")) {
+            const text = data.url.searchParams.get("id");
+            if (text) {
+                model.id = parseInt(text);
+                let resp = await client.findById(model.id, token.getToken());
+                if (resp) {
+                    model.descricao = resp.descricao;
                 }
             }
-        });
-    }
+        }
+    });
 
     function onCancel() {
         goto("/falha");
